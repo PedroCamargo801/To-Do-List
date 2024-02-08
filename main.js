54d62198463485b32f98742590a82b713edd3e95
PedroCamargo801 <pedrocamargo801@gmail.com>
2024-02-08 09:21:58 -0300

    To Do List

---
+++ main.js
@@ -0,0 +1,26 @@
+let input = document.getElementById("input-principal");
+let button = document.getElementById("botao-adicionar");
+let listaCompleta = document.getElementById("tarefas");
+
+let arrayDeTarefas = []
+
+function mostrarNaTela() {
+    let novaLista = " "
+
+    arrayDeTarefas.forEach(tarefa => {
+        novaLista = novaLista + `
+            <li class="item-tarefa">
+                <p>${tarefa}</p>
+            </li>
+        `
+    })
+
+    listaCompleta.innerHTML = novaLista
+}
+
+function cliqueiNoBotao(){
+    arrayDeTarefas.push(input.value);
+    mostrarNaTela()
+}
+
+button.addEventListener("click", cliqueiNoBotao);
